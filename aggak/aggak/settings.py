b40e7b19a5cd4c37fa7e8234077bbf8a9e5d9ed9
aggak <69405480+aggak@users.noreply.github.com>
2021-03-12 00:23:47 +0100

    Update settings.py

--- aggak/aggak/settings.py
+++ aggak/aggak/settings.py
@@ -16,16 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wyx&t!0w5*p1$lbdsra%w+9rce$9oau3s^bz4bh-wkqbkwya6i'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['.pythonanywhere.com',]
+# Quick-start development settings - unsuitable for pr
 
 
 # Application definition
